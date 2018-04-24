2067c0f7041658064520ceda66174d924a5d57c9
Dylan Katz <dylanishappy1@gmail.com>
2018-04-24 13:11:36 -0600

    Made post interval configurable

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import argparse
 from time import sleep
 
+import yaml
+
 from slackposter import CVEPoster
 
 if __name__ == '__main__':
@@ -9,6 +11,15 @@ if __name__ == '__main__':
                            help='Sets the file to pull patterns from (defaults to ".dependencies.txt")')
     args = argparser.parse_args()
     cve_poster = CVEPoster(args.config_file)
+    slack_post_interval = 5
+
+    with open(args.config_file, 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+            slack_post_interval = config.get('slack_post_interval')
+        except:
+            pass
+
     while True:
         cve_poster.post_to_feed_if_needed()
-        sleep(10)
+        sleep(slack_post_interval * 60)
--- slackposter.py
+++ slackposter.py
@@ -48,46 +48,42 @@ class CVEPoster:
 
     def __init__(self, config_file):
         self.config_file = config_file
-        self.last_update = None
         self.cve_list = None
         self.old_cve_list = None
         if os.path.exists('.cve_cache') and os.path.isfile('.cve_cache'):
             with open('.cve_cache') as f:
                 try:
                     self.old_cve_list = json.loads(f.read())
-                except Exception as e:
+                except:
                     self.old_cve_list = None
 
         self.post_to_feed_if_needed()
         self.slack_webhook = None
 
     def post_to_feed_if_needed(self):
-        now = datetime.datetime.now()
-        if self.last_update is None or (now - self.last_update).total_seconds() > 10:
-            with open(self.config_file, 'r') as stream:
-                try:
-                    config = yaml.safe_load(stream)
-                    self.slack_webhook = config.get('slack_webhook')
+        with open(self.config_file, 'r') as stream:
+            try:
+                config = yaml.safe_load(stream)
+                self.slack_webhook = config.get('slack_webhook')
 
-                except yaml.YAMLError as exc:
-                    print(exc)
-                    return
+            except yaml.YAMLError as exc:
+                print(exc)
+                return
 
-                self.cve_list = list(cve for cve in get_cve_generator(config).generate_feed())
+            self.cve_list = list(cve for cve in get_cve_generator(config).generate_feed())
 
-                print('Reloaded CVE feeds and patterns. Posting messages if necessary.')
-                if self.old_cve_list:
-                    diffed_list = list(set(self.cve_list) - set(self.old_cve_list))
-                    for item in diffed_list:
-                        response = requests.post(self.slack_webhook, item)
-                        response.raise_for_status()
-                else:
-                    for item in self.cve_list:
-                        response = requests.post(self.slack_webhook, item)
-                        response.raise_for_status()
+            print('Reloaded CVE feeds and patterns. Posting messages if necessary.')
+            if self.old_cve_list:
+                diffed_list = list(set(self.cve_list) - set(self.old_cve_list))
+                for item in diffed_list:
+                    response = requests.post(self.slack_webhook, item)
+                    response.raise_for_status()
+            else:
+                for item in self.cve_list:
+                    response = requests.post(self.slack_webhook, item)
+                    response.raise_for_status()
 
-                if self.cve_list:
-                    self.old_cve_list = self.cve_list
-                    with open('.cve_cache', 'w+') as f:
-                        f.write(json.dumps(self.old_cve_list))
-            self.last_update = now
+            if self.cve_list:
+                self.old_cve_list = self.cve_list
+                with open('.cve_cache', 'w+') as f:
+                    f.write(json.dumps(self.old_cve_list))
